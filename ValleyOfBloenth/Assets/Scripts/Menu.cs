689a8625bf81cc8a90570e403fc3a0600df5e774
Frazzlectra <baherndon16@tfa.edu>
2015-04-29 11:55:36 -0500

    Setup
    I started a menu system (with a few animations that still need to be set
    up), as well as a first level. For more info on what I plan to do with
    this weeks game you can read the GDD.

---
+++ ValleyOfBloenth/Assets/Scripts/Menu.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour {
+    public Button playBtn;
+    public Button quitBtn;
+
+    Animator anim;
+    bool wait;//want to set load level to go after animations
+    void Awake()
+    {
+        //setting up animator and buttons
+        anim = GetComponent<Animator>();
+        playBtn.onClick.AddListener(() => { ButtonClicked(0); });
+        quitBtn.onClick.AddListener(() => { ButtonClicked(1); });
+    }
+
+    private void ButtonClicked(int btn)
+    {
+        switch (btn)
+        {
+            case 0:
+                anim.SetTrigger("Play");
+                Debug.Log("Play Animations");
+                //want to set load level to go after animations
+                Application.LoadLevel(1);
+                break;
+            case 1:
+                Application.Quit();
+                break;
+            default:
+                Debug.Log("Btn clicked Nope");
+                break;
+        }
+    }
+
+}
